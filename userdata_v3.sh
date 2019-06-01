18e432f15d23c115b2601c1f62aba794a535c789
Bharathkumar <bhrth.dsra1@gmail.com>
2019-06-01 11:35:29 +0800

    Different versions of userdata

---
+++ userdata_v3.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+yum update -y 
+yum install httpd -y
+echo "<html><body><center><h1>Hello Ladies and Gentle Men. Here is my web page!</h1></center></body></html>" > /var/www/html/index.html
+systemctl start httpd
+systemctl enable httpd
